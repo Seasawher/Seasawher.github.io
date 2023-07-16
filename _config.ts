1ce41ef4e5627006a74324284288df149133e158
Seasawher <tellagoodstory@icloud.com>
2023-07-17 01:43:12 +0900

    設定ファイルを編集

--- _config.ts
+++ _config.ts
@@ -1,5 +1,11 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const site = lume({
+    src: "./src",
+    dest: "_site",
+    server: {
+      open: true,
+    },
+});
 
 export default site;
