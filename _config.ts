0983a398b15d39c662fb5595bc93a8ef20ae8efb
Seasawher <tellagoodstory@icloud.com>
2023-07-17 01:52:06 +0900

    lumeのテンプレートを使用する

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog@v0.5.0/mod.ts";
 
 const site = lume({
     src: "./src",
@@ -7,5 +8,7 @@ const site = lume({
       open: true,
     },
 });
+
+site.use(blog());
 
 export default site;
