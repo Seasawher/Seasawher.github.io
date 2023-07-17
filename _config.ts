fd81cedfb23b79abd38b23f1c903590cf324c845
Seasawher <tellagoodstory@icloud.com>
2023-07-17 13:19:09 +0900

    シンタックスハイライトを追加する

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog@v0.5.0/mod.ts";
+import codeHighlight from "lume/plugins/code_highlight.ts";
 
 const site = lume({
     src: "src",
@@ -9,6 +10,12 @@ const site = lume({
     },
 });
 
-site.use(blog());
+site
+  .use(blog())
+  .remoteFile(
+    "_includes/css/code.css",
+    "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github-dark.min.css"
+  )
+  .use(codeHighlight());
 
 export default site;
