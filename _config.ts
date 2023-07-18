31c56d2282e092b0641978f84be13e87df7c19d2
seasawher <tellagoodstory@icloud.com>
2023-07-18 21:26:50 +0900

    日付を日本の形式に合わせる

--- _config.ts
+++ _config.ts
@@ -1,6 +1,8 @@
 import lume from "lume/mod.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog@v0.5.0/mod.ts";
+import date from "lume/plugins/date.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
+import ja from "npm:date-fns/locale/ja/index.js";
 
 const site = lume({
     src: "src",
@@ -17,6 +19,9 @@ site
     // カラーテーマは https://github.com/highlightjs/highlight.js/tree/main/src/styles から好きなものを選ぼう
     "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.6.0/build/styles/github-dark.min.css"
   )
-  .use(codeHighlight());
+  .use(codeHighlight())
+  .use(date({
+    locales: { ja },
+  }));
 
 export default site;
